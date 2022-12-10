cfac04eb1ace963230a25d17e7d829ea8e6d24e2
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-12-10 13:39:36 -0300

    delete adaptador debug signals

--- src/adaptador/adaptador_entrada.vhd
+++ src/adaptador/adaptador_entrada.vhd
@@ -18,12 +18,6 @@ ENTITY adaptador_entrada IS
         rx_os_data_o : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
         rx_os_dv_o : OUT STD_LOGIC;
         rx_os_rfd_i : IN STD_LOGIC;
-
-        -- Modulator
-        rx_ovf_o : OUT STD_LOGIC;
-
-        -- Control
-        send_o : OUT STD_LOGIC
     );
 END ENTITY adaptador_entrada;
 
@@ -42,7 +36,6 @@ ARCHITECTURE rtl OF adaptador_entrada IS
     SIGNAL rx_os_data_s : STD_LOGIC_VECTOR(7 DOWNTO 0);
     SIGNAL rx_os_dv_s : STD_LOGIC;
     SIGNAL rx_ovf_s : STD_LOGIC;
-    SIGNAL send_s : STD_LOGIC;
 
 BEGIN
 
@@ -51,7 +44,6 @@ BEGIN
         IF srst_i = '1' THEN
             rx_os_dv_s <= '0';
             rx_ovf_s <= '0';
-            send_s <= '0';
 
             output_candidate_s <= (OTHERS => '0');
             rx_os_data_s <= (OTHERS => '0');
@@ -97,41 +89,5 @@ BEGIN
     rx_os_data_o <= rx_os_data_s;
     rx_os_dv_o <= rx_os_dv_s;
     rx_ovf_o <= rx_ovf_s;
-    send_o <= send_s;
 
 END ARCHITECTURE rtl;
-
-
---   u_send_logic : process(clk_i)
---   begin
---     if rising_edge(clk_i) then
---       if srst_s = '1' then
---         pipe_data_counter_s <= (others => '0');
---         modem_send_s <= '0';
---         -- modem_tx_rdy_d10_s <= (others => '0');
---       else
---         if  uart_os_dv_s   = '1' and
---             uart_os_rfd_s  = '1' and
---             modem_is_dv_s  = '1' and
---             modem_is_rfd_s = '1'
---         then
---           pipe_data_counter_s <= pipe_data_counter_s;
---         elsif  modem_is_dv_s  = '1' and
---                modem_is_rfd_s = '1'
---         then
---           pipe_data_counter_s <= std_logic_vector(unsigned(pipe_data_counter_s)-1);
---         elsif  uart_os_dv_s   = '1' and
---                uart_os_rfd_s  = '1'
---         then
---           pipe_data_counter_s <= std_logic_vector(unsigned(pipe_data_counter_s)+1);
---         end if;
---         if modem_send_s = '1' then
---           modem_send_s <= '0';
---         else
---           if unsigned(pipe_data_counter_s) > unsigned(nm1_bytes_i) and modem_tx_rdy_s = '1' then
---             modem_send_s <= '1';
---           end if;
---         end if;
---       end if;
---     end if;
---   end process;
